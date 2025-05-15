ecc9d72b223b2b1c38f7e5ae94efb687af92b53e
Abhi-Jeeet <abhijeetkumar3245@gmail.com>
2025-05-15 23:19:07 +0530

    Create A_Easy_Problem.cpp

---
+++ A_Easy_Problem.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int Cpair(int n){
+    return n-1;
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<Cpair(n)<<endl;
+    }
+    
+}
